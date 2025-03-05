6cd4042a44071964ddc628ac439c971142e94842
2216065 <2216065@leedstrinity.ac.uk>
2025-03-05 15:02:51 +0000

    added the Lobby page

--- Backend/Mark's Functions/Test.js
+++
@@ -1,6 +0,0 @@
-function Test(mapID) {
-    let mapIDString = mapID.toString();
-    let url = 'http://trinity-developments.co.uk/games/' + mapIDString;
-    console.log(url);
-}
-Test(1);
--- Frontend/README.md
+++
@@ -1,1 +0,0 @@
-# Cloud-Commanders-Horsforth-Hunt
