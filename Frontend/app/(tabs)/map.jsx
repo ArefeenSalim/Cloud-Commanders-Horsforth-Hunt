2994126242e0ca757129baca379fffb204cc73c2
ArefeenSalim <2303968@leedstrinity.ac.uk>
2025-03-06 11:18:59 +0000

    maps page is in progress

--- Frontend/app/(tabs)/map.jsx
+++ Frontend/app/(tabs)/map.jsx
@@ -12,12 +12,16 @@ const MapPage = () => {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handlePress1}>
-        <Text style={styles.buttonText}>Dr X movements</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handlePress2}>
-        <Text style={styles.buttonText}>Tickets</Text>
-      </TouchableOpacity>
+      <View style={styles.leftButtonContainer}>
+        <TouchableOpacity style={styles.drXButton} onPress={handlePress1}>
+          <Text style={styles.buttonText}>Dr X movements</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomButtonContainer}>
+        <TouchableOpacity style={styles.ticketsButton} onPress={handlePress2}>
+          <Text style={styles.buttonText}>Tickets</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -25,28 +29,42 @@ const MapPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#000000',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: '#DDDD91',
-    padding: 15,
-    borderRadius: 10,
-    marginVertical: 10,
-    width: 200,
-    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
   },
   buttonText: {
     color: '#000000',
-    fontSize: 16,
+    fontSize: 5,
     fontWeight: 'bold',
+  },
+  drXButton: {
+    backgroundColor: '#ff7f7f', // Red color for "Dr X movements" button
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: 50,
+    alignItems: 'center',
+  },
+  ticketsButton: {
+    backgroundColor: '#add8e6', // Blue color for "Tickets" button
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 10,
+    width: 50,
+    alignItems: 'center',
+  },
+  leftButtonContainer: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flex: 1, // This makes the container take up all available space vertically
+  },
+  bottomButtonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
   },
 });
 
 export default MapPage;
+
